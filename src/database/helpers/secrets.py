681c3eb02958cea9374d535ddeb964788a48e461
Pedro Cunha <a45760@alunos.ipb.pt>
2020-12-06 01:13:21 +0000

    Inital development stage version beta

--- src/database/helpers/secrets.py
+++ src/database/helpers/secrets.py
@@ -1,13 +1,15 @@
-DB_USER = 'cunha'
-DB_PASS = '{g7e(:^sJeKd6S)q8pVXJLSf{Y5uS_'
-DB_HOST = '185.83.212.53'
-DB_PORT = 3306
-DB_NAME = 'cunhaacNET'
+import os
 
-MAIL_SERVER   = 'smtp.sapo.pt'
-MAIL_PORT     = 587
-MAIL_USE_TLS  = True
-MAIL_USERNAME = 'testing_your_ass@sapo.pt'
-MAIL_PASSWORD = 'qQwstSD6'
+DB_USER = os.environ.get('MYSQL_USER')
+DB_PASS = os.environ.get('MYSQL_PASSWORD')
+DB_HOST = os.environ.get('MYSQL_USER')
+DB_PORT = os.environ.get('MYSQL_PORT')
+DB_NAME = os.environ.get('MYSQL_DATABASE')
 
-SECRET_KEY = 'aoisdjas329874ijadaksd293874eaujskdsdiuasdhasd'
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = os.environ.get('MAIL_PORT')
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
